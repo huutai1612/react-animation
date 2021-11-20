908b8db29c55851995dad233ddc4d93c62ca0196
huutai1612 <tai.huu1612@gmail.com>
2021-11-20 14:30:59 +0700

    Use transition group to group CssTransition component

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Transition, CSSTransition } from 'react-transition-group';
+import { /*Transition, */ CSSTransition } from 'react-transition-group';
 
 import './Modal.css';
 
