4ac2a92ac9656e2b9e39d814ae5379023b0f3d90
huutai1612 <tai.huu1612@gmail.com>
2021-11-20 11:37:06 +0700

    add Transition component to delay un mount component from react

--- src/App.js
+++ src/App.js
@@ -1,40 +1,32 @@
-import React, { Component } from 'react';
-
+import React, { useState } from 'react';
+import { Transition } from 'react-transition-group';
 import './App.css';
 import Modal from './components/Modal/Modal';
 import Backdrop from './components/Backdrop/Backdrop';
 import List from './components/List/List';
 
-class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			isShown: false,
-		};
-	}
+const App = () => {
+	const [isShow, setIsShow] = useState(false);
 
-	closeModal = () => {
-		this.setState({ isShown: false });
-	};
+	const showModal = () => setIsShow(true);
 
-	showModal = () => {
-		this.setState({ isShown: true });
-	};
+	const closeModal = () => setIsShow(false);
 
-	render() {
-		return (
-			<div className='App'>
-				<h1>React Animations</h1>
-				<Modal show={this.state.isShown} closed={this.closeModal} />
-				<Backdrop show={this.state.isShown} closed={this.closeModal} />
-				<button className='Button' onClick={this.showModal}>
-					Open Modal
-				</button>
-				<h3>Animating Lists</h3>
-				<List />
-			</div>
-		);
-	}
-}
+	return (
+		<div className='App'>
+			<h1>React Animations</h1>
+			<Transition in={isShow} timeout={500} mountOnEnter unmountOnExit>
+				{(state) => <Modal show={state} closed={closeModal} />}
+			</Transition>
+
+			{isShow && <Backdrop show={isShow} closed={closeModal} />}
+			<button className='Button' onClick={showModal}>
+				Open Modal
+			</button>
+			<h3>Animating Lists</h3>
+			<List />
+		</div>
+	);
+};
 
 export default App;
