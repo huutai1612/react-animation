e57652f98299f87918e38ed015882c4123e5658b
huutai1612 <tai.huu1612@gmail.com>
2021-11-20 13:51:14 +0700

    using CssTransistion component we edit classname

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,39 +1,47 @@
 import React from 'react';
-import { Transition } from 'react-transition-group';
+import { Transition, CSSTransition } from 'react-transition-group';
 
 import './Modal.css';
 
 const modal = (props) => {
-	const cssClasses = `Modal ${
-		props.show === 'entering'
-			? 'ModalOpen'
-			: props.show === 'exiting'
-			? 'ModalClose'
-			: null
-	}`;
-
 	return (
-		<Transition in={props.show} timeout={500} mountOnEnter unmountOnExit>
-			{(state) => {
-				const cssClasses = `Modal ${
-					state === 'entering'
-						? 'ModalOpen'
-						: state === 'exiting'
-						? 'ModalClose'
-						: null
-				}`;
-
-				return (
-					<div className={cssClasses}>
-						<h1>A Modal</h1>
-						<button className='Button' onClick={props.closed}>
-							Dismiss
-						</button>
-					</div>
-				);
-			}}
-		</Transition>
+		<CSSTransition
+			in={props.show}
+			timeout={500}
+			classNames='modal'
+			mountOnEnter
+			unmountOnExit>
+			<div className='modal'>
+				<h1>A Modal</h1>
+				<button className='Button' onClick={props.closed}>
+					Dismiss
+				</button>
+			</div>
+		</CSSTransition>
 	);
+
+	// return (
+	// 	<Transition in={props.show} timeout={500} mountOnEnter unmountOnExit>
+	// 		{(state) => {
+	// 			const cssClasses = `Modal ${
+	// 				state === 'entering'
+	// 					? 'ModalOpen'
+	// 					: state === 'exiting'
+	// 					? 'ModalClose'
+	// 					: null
+	// 			}`;
+
+	// 			return (
+	// <div className={cssClasses}>
+	// 	<h1>A Modal</h1>
+	// 	<button className='Button' onClick={props.closed}>
+	// 		Dismiss
+	// 	</button>
+	// </div>
+	// 			);
+	// 		}}
+	// 	</Transition>
+	// );
 };
 
 export default modal;
