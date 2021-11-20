8bdc7e6d98d177b2ec76f6e1ccb7f244b4463efb
huutai1612 <tai.huu1612@gmail.com>
2021-11-20 14:16:58 +0700

    we can modify CssTransition className by add to it an object

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -9,48 +9,53 @@ const animationTiming = {
 };
 
 const modal = (props) => {
-	// return (
-	// 	<CSSTransition
-	// 		in={props.show}
-	// 		timeout={500}
-	// 		classNames='modal'
-	// 		mountOnEnter
-	// 		unmountOnExit>
-	// 		<div className='modal'>
-	// 			<h1>A Modal</h1>
-	// 			<button className='Button' onClick={props.closed}>
-	// 				Dismiss
-	// 			</button>
-	// 		</div>
-	// 	</CSSTransition>
-	// );
-
 	return (
-		<Transition
+		<CSSTransition
 			in={props.show}
 			timeout={animationTiming}
+			classNames={{
+				enter: '',
+				enterActive: 'ModalOpen',
+				exit: '',
+				exitActive: 'ModalClose',
+			}}
 			mountOnEnter
 			unmountOnExit>
-			{(state) => {
-				const cssClasses = `Modal ${
-					state === 'entering'
-						? 'ModalOpen'
-						: state === 'exiting'
-						? 'ModalClose'
-						: null
-				}`;
-
-				return (
-					<div className={cssClasses}>
-						<h1>A Modal</h1>
-						<button className='Button' onClick={props.closed}>
-							Dismiss
-						</button>
-					</div>
-				);
-			}}
-		</Transition>
+			<div className='modal'>
+				<h1>A Modal</h1>
+				<button className='Button' onClick={props.closed}>
+					Dismiss
+				</button>
+			</div>
+		</CSSTransition>
 	);
+
+	// return (
+	// 	<Transition
+	// 		in={props.show}
+	// 		timeout={animationTiming}
+	// 		mountOnEnter
+	// 		unmountOnExit>
+	// 		{(state) => {
+	// 			const cssClasses = `Modal ${
+	// 				state === 'entering'
+	// 					? 'ModalOpen'
+	// 					: state === 'exiting'
+	// 					? 'ModalClose'
+	// 					: null
+	// 			}`;
+
+	// 			return (
+	// 				<div className={cssClasses}>
+	// 					<h1>A Modal</h1>
+	// 					<button className='Button' onClick={props.closed}>
+	// 						Dismiss
+	// 					</button>
+	// 				</div>
+	// 			);
+	// 		}}
+	// 	</Transition>
+	// );
 };
 
 export default modal;
